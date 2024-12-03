bcb6431188ad20137181d8f560934a9fbb11a388
Nishant Buwa <nishantbuwa01@gmail.com>
2024-12-03 14:36:45 +0530

    Update .gitignore to exclude .env file

--- src/Components/RandomQuote.jsx
+++ src/Components/RandomQuote.jsx
@@ -1,62 +1,8 @@
-// import React, { useState } from 'react'
-// import './RandomQuote.css'
-// import twitter from './Assests/twitter.png'
-// import reload from './Assests/reload.png'
-
-// const RandomQuote = () => {
-
-//     let quotes=[]
-
-//     async function loadQuote(){
-//         const response=await fetch("https://type.fit/api/quotes")
-//         quotes=await response.json()
-//     }
-
-
-//     const [quote,setQuote]=useState({
-//         text:"Difficulties increase the nearer we get to the goal",
-//         author:"Johann Wolfgang Von Goethe"
-//     })
-
-//     const random=()=>{
-//         const select=quotes[Math.floor(Math.random()*quotes.length)]
-//         setQuote(select)
-//     }
-
-//     const twitter_button=()=>{
-//         window.open(`https://twitter.com/intent/tweet?text=${quote.text}`)
-//     }
-//     loadQuote()
-
-//     return (
-//         <div className="container">
-//         <div className="quote">
-//             {quote.text}
-//         </div>
-//         <div>
-//             <div className="line-cover">
-//                 <div className="line"></div>
-//             </div>
-//             <div className="bottom">
-//                 <div className="author">
-//                     - {quote.author.split(',')[0]}
-//                 </div>
-//                 <div className="icons">
-//                     <img src={reload} alt="" onClick={()=>{random()}}/>
-//                     <img src={twitter} alt="" onClick={()=>{twitter_button()}} />
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default RandomQuote
-
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './RandomQuote.css';
 import twitter from './Assests/twitter.png';
 import reload from './Assests/reload.png';
+
 
 const RandomQuote = () => {
     const categories = [
@@ -80,7 +26,7 @@ const RandomQuote = () => {
             const response = await fetch(
                 `https://api.api-ninjas.com/v1/quotes?category=${randomCategory}`,
                 {
-                    headers: { 'X-Api-Key': 'aDkYjmDEvh4TBrqerGV6mw==6yFJwKQs9lVOPXia' },
+                    headers: { 'X-Api-Key': process.env.REACT_APP_API_KEY },
                 }
             );
             const result = await response.json();
@@ -92,10 +38,7 @@ const RandomQuote = () => {
         }
     };
 
-    useEffect(() => {
-        loadQuote(); // Load the initial quote on component mount
-    }, []);
-
+    
     const twitter_button = () => {
         window.open(`https://twitter.com/intent/tweet?text=${quote.quote}`);
     };
